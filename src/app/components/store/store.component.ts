922a175dc783ccd66b5f7e9c47aba256556a8b74
ilyas ahmed <ilyasahmed081@gmail.com>
2021-08-04 22:51:15 +0500

    header carousel game cards added

--- src/app/components/store/store.component.ts
+++ src/app/components/store/store.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StoreComponent implements OnInit {
 
+  list: any = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.list = [
+      { image: "assets/img/g1.jpg", name: "Assassins Creed" },
+      { image: "assets/img/g2.jpeg", name: "Nier Automata" },
+      { image: "assets/img/g3.jpg", name: "Watch Dogs" },
+      { image: "assets/img/g5.jpg", name: "Control" },
+      { image: "assets/img/g6.jpg", name: "Watch Dogs 2" },
+      { image: "assets/img/g1.jpg", name: "Assassins Creed" },
+      { image: "assets/img/g2.jpeg", name: "Nier Automata" },
+      { image: "assets/img/g3.jpg", name: "Watch Dogs" },
+    ]
   }
 
 }
