4b9479af7ced7eeb2840a591e5f3e43aa8ad972c
ilyas ahmed <ilyasahmed081@gmail.com>
2021-08-04 20:36:08 +0500

    header added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HeaderComponent } from './layout/header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './layout/header/header.component';
 
 @NgModule({
   declarations: [
